ca7c560a5193644a2e3bdd347e2d6ce30fa6b980
belo <pesadam15@gmail.com>
2023-12-20 23:43:14 +0100

    vypisovanie z db, about stranka

--- score/models.py
+++ score/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
