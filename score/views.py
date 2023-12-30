61d6e64f2befa43cae880172e8d905fc7c61a68b
belo <pesadam15@gmail.com>
2023-12-30 16:49:21 +0100

    save, delete,

--- score/views.py
+++ score/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from score.models import Score
+from score.forms import ScoreForm
 
 def index(request):
     context = {}
+    form = ScoreForm()
     scores = Score.objects.all()
     context['scores'] = scores
     context['title'] = 'Home'
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            form = ScoreForm(request.POST)
+            form.save()
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            score = Score.objects.get(id=pk)
+            score.delete()
+        elif 'edit' in request.POST:
+            pk = request.get('edit')
+            score = Score.objects.get(id=pk)
+            form  = ScoreForm(instance=score)
+    context['form'] = form
     return render(request, 'index.html', context)
 
 def about(request):
