624a303cc7d58afbc344b08e0df902b30c093768
belo <pesadam15@gmail.com>
2024-02-03 11:38:14 +0100

    .

--- score/urls.py
+++ score/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from register import views as v
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('register/', v.register, name='register')
 ]
